91cf0d369ac7dc630e444dc3e983af55185a81cf
Frank Yang <36429488+FrankYang0610@users.noreply.github.com>
2024-07-16 17:32:32 +0800

    Add files via upload

---
+++ Terminal/Error_Stream/Error_Stream.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Frank Yang on 5/4/24.
+//
+
+#include "Error_Stream.h"
+
+void print_error(const char* error) {
+    std::cerr << "ERR: " << error << std::endl;
+}
+
---
+++ Terminal/Error_Stream/Error_Stream.h
@@ -0,0 +1,12 @@
+//
+// Created by Frank Yang on 5/4/24.
+//
+
+#ifndef ERROR_STREAM_H
+#define ERROR_STREAM_H
+
+#include <iostream>
+
+void print_error(const char* error);
+
+#endif //ERROR_STREAM_H
